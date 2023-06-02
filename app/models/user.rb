007874c975506978735503944591de992abd98e5
yuhmanashi <yzxu28@gmail.com>
2023-06-01 23:37:35 -0700

    updated models

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+
+    has_many :posts,
+        foreign_key: :user_id,
+        class_name: :Post
+    
+    has_many :comments,
+        foreign_key: :user_id,
+        class_name: :Comment
+end
