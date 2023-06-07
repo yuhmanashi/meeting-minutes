ba7aada4ca8f4bdb8b4728534e2e486cb7038b6b
yuhmanashi <yzxu28@gmail.com>
2023-06-06 23:46:30 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:index, :create, :show, :update]
+  end
   root to: 'static_pages#root'
 end
