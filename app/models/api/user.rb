2b545a950eac10aa8e02f8fc68a95db88cd548bf
yuhmanashi <yzxu28@gmail.com>
2023-06-07 22:32:00 -0700

    finished user model

--- app/models/api/user.rb
+++ app/models/api/user.rb
@@ -1,10 +1,12 @@
 class Api::User < ApplicationRecord
     has_secure_password
-
+    
     validates :username, :email, uniqueness: true
     validates :password_digest, presence: true
     validates :session_token, presence: true, uniqueness: true
-    validates :password, length { in: 6..16 }, allow_nil: true
+    validates :password, length: { in: 6..16 }, allow_nil: true
+
+    after_initialize :ensure_session_token
 
     has_many :posts,
         foreign_key: :user_id,
@@ -13,4 +15,15 @@ class Api::User < ApplicationRecord
     has_many :comments,
         foreign_key: :user_id,
         class_name: :Comment
+    
+    def reset_session_token!
+        self.session_token = SecureRandom.base64(64)
+        self.save!
+        self.session_token
+    end
+    
+    private
+    def ensure_session_token
+        self.session_token ||= SecureRandom.base64(64)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:index, :create, :show, :update]
+    resources :users, only: [:index, :create, :show]
+    resources :posts, only: [:index, :show, :create, :update, :destroy]
+    resources :comments, only: [:index, :show, :create, :update, :destroy]
   end
   root to: 'static_pages#root'
 end
