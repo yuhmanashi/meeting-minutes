c540acdc3336fe8197def065e6eebb4f458d3c35
yuhmanashi <yzxu28@gmail.com>
2023-07-28 22:57:22 -0700

    working on history

--- app/frontend/src/components/History/History.jsx
+++ app/frontend/src/components/History/History.jsx
@@ -1,3 +1,46 @@
-export default function History(){
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import './Home.css';
+import { useAppDispatch, useAppSelector } from '../../utils/hooks';
 
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+import Meetings from '../Meetings/Meetings';
+import Button from '@mui/material/Button';
+
+import * as meetingActions from '../../store/meetings';
+import * as studentActions from '../../store/students';
+
+import CreateMeetingModal from '../Meetings/CreateMeetingModal';
+import GenericChart from '../CommonComponents/Chart';
+
+export default function History(){
+    const sessionUser = useAppSelector(state => state.session.user);
+    const sessionMeetings = useAppSelector((state) => state.meetings);
+    const sessionStudents = useAppSelector((state) => state.students);
+
+    const dispatch = useAppDispatch();
+
+    if (Object.keys(sessionMeetings).length < 1 || Object.keys(sessionStudents).length < 1) return null;
+
+    useEffect(() => {
+        dispatch(meetingActions.fetchMeetings());
+        dispatch(studentActions.fetchStudents());
+    }, [dispatch])
+
+    return (
+        <Box>
+            {/* Charts */}
+            <Container>
+                {/* Charts go here */}
+            </Container>
+            {/* Data */}
+            <Container>
+                {/* AutoComplete for toggle */}
+                {/* Table */}
+            </Container>
+        </Box>
+    )
 };
