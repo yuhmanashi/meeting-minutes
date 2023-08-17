60f82b0f50fb82adadbaae5c08093169bd6c3e83
yuhmanashi <yzxu28@gmail.com>
2023-08-16 18:10:34 -0700

    removed migrations

--- db/migrate/20230628015352_add_email_to_meeting.rb
+++
@@ -1,6 +0,0 @@
-class AddEmailToMeeting < ActiveRecord::Migration[7.0]
-  def change
-    add_column :meetings, :student_email, :string, null: false
-    add_index :meetings, :student_email
-  end
-end
--- db/migrate/20230711004719_dropmeetingstable.rb
+++
@@ -1,5 +0,0 @@
-class Dropmeetingstable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :meetings
-  end
-end
